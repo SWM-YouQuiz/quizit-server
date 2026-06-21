1410343d0a2ae278e0d141f6e391f9a0967fe145
earlgrey02 <san06036@naver.com>
2026-06-21 10:35:52 +0900

    feat: 퀴즈 채점 기능 구현

---
+++ core/src/main/kotlin/com/quizit/core/domain/quiz/dto/result/GradeQuizResult.kt
@@ -0,0 +1,6 @@
+package com.quizit.core.domain.quiz.dto.result
+
+data class GradeQuizResult(
+    val isCorrect: Boolean,
+    val solution: String
+)
--- core/src/main/kotlin/com/quizit/core/domain/quiz/service/QuizService.kt
+++ core/src/main/kotlin/com/quizit/core/domain/quiz/service/QuizService.kt
@@ -1,10 +1,17 @@
 package com.quizit.core.domain.quiz.service
 
+import com.quizit.core.domain.quiz.dto.command.GradeQuizCommand
 import com.quizit.core.domain.quiz.dto.query.GetSolvedQuizzesQuery
 import com.quizit.core.domain.quiz.dto.result.GetQuizResult
 import com.quizit.core.domain.quiz.dto.result.GetSolvedQuizResult
+import com.quizit.core.domain.quiz.dto.result.GradeQuizResult
+import com.quizit.core.domain.quiz.exception.QuizNotFoundException
+import com.quizit.core.domain.quiz.exception.QuizOptionNotFoundException
 import com.quizit.core.domain.quiz.repository.QuizOptionRepository
 import com.quizit.core.domain.quiz.repository.QuizRepository
+import com.quizit.core.domain.user.entity.UserSolvedQuiz
+import com.quizit.core.domain.user.repository.UserSolvedQuizRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -12,7 +19,8 @@ import java.util.*
 @Service
 class QuizService(
     private val quizRepository: QuizRepository,
-    private val quizOptionRepository: QuizOptionRepository
+    private val quizOptionRepository: QuizOptionRepository,
+    private val userSolvedQuizRepository: UserSolvedQuizRepository
 ) {
     @Transactional(readOnly = true)
     fun getQuizzesByChapterId(
@@ -73,5 +81,33 @@ class QuizService(
                 options = quizOptions.getValue(it.id)
             )
         }
+    }
+
+    @Transactional
+    fun gradeQuiz(
+        userId: UUID,
+        command: GradeQuizCommand
+    ): GradeQuizResult {
+        val quiz = quizRepository.findByIdOrNull(command.quizId) ?: throw QuizNotFoundException()
+        val option =
+            quizOptionRepository.findByIdAndQuizId(command.selectedOptionId, command.quizId)
+                ?: throw QuizOptionNotFoundException()
+        val isSolved = userSolvedQuizRepository.existsByUserIdAndQuizId(userId, command.quizId)
+
+        if (!isSolved) {
+            userSolvedQuizRepository.save(
+                UserSolvedQuiz(
+                    userId = userId,
+                    quizId = command.quizId,
+                    selectedOptionId = command.selectedOptionId,
+                    isCorrect = option.isAnswer
+                )
+            )
+        }
+
+        return GradeQuizResult(
+            isCorrect = option.isAnswer,
+            solution = quiz.solution
+        )
     }
 }
